cdf53c8ee7fb2dac67aeef6e84ff5b9315df2f96
s457232 <55598449+s457232@users.noreply.github.com>
2019-11-05 14:11:56 -0500

    Added new code for assignment

--- SplashScreenFerdous/main.lua
+++ SplashScreenFerdous/main.lua
@@ -5,3 +5,17 @@
 -----------------------------------------------------------------------------------------
 
 -- Your code here
+
+
+-- hide the status bar
+display.setStatusBar(display.HiddenStatusBar)
+
+-----------------------------------------------------------------------------------------
+
+-- Use composer library
+local composer = require( "composer" )
+
+-----------------------------------------------------------------------------------------
+
+-- Go to the intro screen
+composer.gotoScene( "splash_screen" )
