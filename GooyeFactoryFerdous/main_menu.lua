9b9423f92412f5f9960a3f165db0865c991ef0a7
Ferdous <ferdous.sediqi@ocsbstudent.ca>
2019-11-20 16:37:34 -0500

    added new pages to assignment

--- GooyeFactoryFerdous/main_menu.lua
+++ GooyeFactoryFerdous/main_menu.lua
@@ -7,90 +7,65 @@
 local composer = require( "composer" )
 
 display.setDefault("background", 0, 0, 0)
-
 -----------------------------------------------------------------------------------------
-
 -- Use Widget Library
 local widget = require( "widget" )
-
 -----------------------------------------------------------------------------------------
-
 local scene = composer.newScene( mainMenu )
  
 function scene:show( event )
-
 -- Creating a group that associates objects with the scene
     local sceneGroup = self.view
 
 -----------------------------------------------------------------------------------------
     local phase = event.phase
 
------------------------------------------------------------------------------------------
-
 -- Called when the scene is still off screen (but is about to come on screen).   
     if ( phase == "will" ) then
-
 -- The function that will go to the main menu 
-
------------------------------------------------------------------------------------------
 
 -- Called when the scene is now on screen.
     elseif ( phase == "did" ) then
 
 -- Insert code here to make the scene come alive.
 -- Example: start timers, begin animation, play audio, etc.     
-
     end
-
-    end -- function scene:show( event )
+end -- function scene:show( event )
 
 -----------------------------------------------------------------------------------------
 
 -- The function called when the scene is issued to leave the screen
 function scene:hide( event )
-
--- Creating a group that associates objects with the scene
     local sceneGroup = self.view
-
------------------------------------------------------------------------------------------
-
     local phase = event.phase
-
-    -----------------------------------------------------------------------------------------
 
     if ( phase == "will" ) then
         -- Called when the scene is on screen (but is about to go off screen).
         -- Insert code here to "pause" the scene.
         -- Example: stop timers, stop animation, stop audio, etc.
 
-    -----------------------------------------------------------------------------------------
-
     elseif ( phase == "did" ) then
         -- Called immediately after scene goes off screen.
     end
 
-    end -- function scene:hide( event )
+end -- function scene:hide( event )
 
 -----------------------------------------------------------------------------------------
 
 -- The function called when the scene is issued to be destroyed
 function scene:destroy( event )
-
-    -- Creating a group that associates objects with the scene
+ -- Creating a group that associates objects with the scene
     local sceneGroup = self.view
 
     -- Called prior to the removal of scene's view ("sceneGroup").
     -- Insert code here to clean up the scene.
     -- Example: remove display objects, save state, etc.
-
 end -- function scene:destroy( event )
 
 scene:addEventListener( "create", scene )
 scene:addEventListener( "show", scene )
 scene:addEventListener( "hide", scene )
 scene:addEventListener( "destroy", scene )
-
 -----------------------------------------------------------------------------------------
-
 return scene
 
