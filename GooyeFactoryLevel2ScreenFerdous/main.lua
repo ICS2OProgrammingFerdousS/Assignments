e7ba0524482877180fb3ecb86ce7f133fe56f42a
s457232 <55598449+s457232@users.noreply.github.com>
2019-12-11 13:23:09 -0500

    finishingsw the assignment

--- GooyeFactoryLevel2ScreenFerdous/main.lua
+++ GooyeFactoryLevel2ScreenFerdous/main.lua
@@ -19,7 +19,7 @@ local composer = require( "composer" )
 -----------------------------------------------------------------------------------------
 
 -- Go to the intro screen
-composer.gotoScene( "Level2_screen")
+composer.gotoScene( "level2_screen")
 --display.setDefault("background", 0.5, 0.7, 1)
 
 
